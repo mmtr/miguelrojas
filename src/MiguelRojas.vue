e152291b7163cf0986492e25b18f0cd1de780054
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-02-24 19:32:42 +0100

    adding header

--- src/MiguelRojas.vue
+++ src/MiguelRojas.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="miguel-rojas">
-    <header class="miguel-rojas__header">
-      <a href="#">
-        <img src="@/assets/miguelrojas.png" alt="Miguel Rojas">
-      </a>
+    <header class="header">
+      <router-link to="home" class="header__logo-link">
+        <img src="@/assets/miguelrojas.png" alt="Miguel Rojas" class="header__logo">
+      </router-link>
     </header>
     <router-view/>
   </div>
@@ -37,5 +37,23 @@
     text-align: left;
     background: #2f2f2f url('~@/assets/bg.svg') no-repeat center center fixed;
     background-size: cover;
+  }
+
+  .header {
+    position: fixed;
+    z-index: 1;
+    padding: 1rem;
+    width: 100%;
+    background-color: rgba(#2f2f2f, 0.8);
+    box-shadow: 0 0 10px #1f1f1f;
+
+    .header__logo-link {
+      display: inline-block;
+    }
+
+    .header__logo {
+      max-height: 30px;
+      vertical-align: middle;
+    }
   }
 </style>
